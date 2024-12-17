9464725d8a758dc2c6b0428b2df41b43bf9a7780
ezuryn <66661592+ezuryn@users.noreply.github.com>
2024-12-17 18:03:44 +0800

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
